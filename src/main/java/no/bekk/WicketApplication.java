e0bf21a1693e2581c05c2028a7ec2c6d865581ec
Peter Hemmen <peter.hemmen@gmail.com>
2013-04-02 22:31:29 +0200

    Ryddet litt opp i urler

--- src/main/java/no/bekk/WicketApplication.java
+++ src/main/java/no/bekk/WicketApplication.java
@@ -6,7 +6,7 @@ import org.apache.wicket.protocol.http.WebApplication;
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
- * @see no.bekk.Start#main(String[])
+ * see the Start class in the test folder
  */
 public class WicketApplication extends WebApplication
 {    	
@@ -28,8 +28,7 @@ public class WicketApplication extends WebApplication
 		super.init();
 
         mountPage("/hello", HelloWorld.class);
-
-        //TODO:  Bruk en URLCodingStrategy så man ikke får så schtøgge urler
+        mountPage("/somethingmounted", AnotherPage.class);
 
 		// add your configuration here
 	}
